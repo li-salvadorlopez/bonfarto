e779c4378fe2da4aedcc26bef977926a5fed402b
salvador lopez <li.salvadorlopez@gmail.com>
2024-06-21 14:06:01 -0600

    Added getters and renamed the postal code property to zipcode

--- doctor-service/src/main/java/com/azteklabs/doctorservice/domain/model/Address.java
+++ doctor-service/src/main/java/com/azteklabs/doctorservice/domain/model/Address.java
@@ -5,14 +5,34 @@ public class Address {
     private final String street;
     private final String city;
     private final String state;
-    private final String postalCode;
+    private final String zipCode;
     private final String country;
 
-    public Address(String street, String city, String state, String postalCode, String country) {
+    public Address(String street, String city, String state, String zipCode, String country) {
         this.street = street;
         this.city = city;
         this.state = state;
-        this.postalCode = postalCode;
+        this.zipCode = zipCode;
         this.country = country;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public String getCountry() {
+        return country;
     }
 }
